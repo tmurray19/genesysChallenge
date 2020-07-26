a6e9457f254935536f5c34dd530f99d57ba6ad12
Taidgh Murray <me@taidghmurray.ie>
2020-07-26 13:29:19 +0100

    Updating game logic to record current turn

--- connect_five_logic.py
+++ connect_five_logic.py
@@ -1,48 +1,71 @@
 # Define constants to print out board to players
-EMPTY = '[ ]'
-RED = '[X]'
-YELLOW = '[Y]'
 
 
 class Game:
-    """ Game class will contain game logic;  
+    """ 
+    Game class will contain game logic;  
         - placing chips into board
         - checking board for winner
         - building and storing board memory
         - checking board for winner
-        """
+    """
 
     def __init__(self):
         """Constructor defines amount of rows, columns, and the board as a 2d array"""
         self.columns = 9
-        self.rows = 6
+        self.rows = 6 
+        self.has_winner = False
+        self.current_turn = 'RED'
+        self.chips = {
+            "EMPTY" : '[ ]',
+            "RED": '[X]',
+            "YELLOW": '[O]'
+        }
         # Create an empty board 
-        self.board = [[EMPTY] * self.rows for _ in range(self.columns)]
+        self.board = [[self.chips.get("EMPTY")] * self.rows for _ in range(self.columns)]
     
-    def insert_chip(self, column, chip):
+    def get_has_winner(self):
+        return self.has_winner
+
+    def get_current_turn(self):
+        return self.current_turn
+
+    def insert_chip(self, column, colour):
+        print(f'{self.get_current_turn()} is making a move')
         """Drops a specific chip into a specified column"""
         col = self.board[column]
 
+        chip = self.chips.get(colour)
+
         # If the top of the column is not empty, we can't place a chip here
-        if col[0] != EMPTY:
+        if col[0] != self.chips.get("EMPTY"):
+            print("Column is full")
             return "Column is full, try again"
         
         # Move through the column top to bottom until an empty slot is found
         i = -1
-        while col[i] != EMPTY:
+        while col[i] != self.chips.get("EMPTY"):
             i -= 1
+        # Place chip in empty slot
         col[i] = chip
 
-        self.check_for_winner(chip)
+        # Check to see if the winner has been found
+        if(self.check_for_winner(chip)):
+            return f"{colour} has won!!"
+        
+        self.current_turn = 'RED' if colour=='YELLOW' else 'YELLOW'
 
     def print_board(self):
         """Builds board and prints in console"""
+        board = ""
         for row in range(self.rows):
-            print(
+            board += (
                 ' '.join(
                     str(self.board[col][row]) for col in range(self.columns)
                 )
             )
+            board += '\n'
+        return board
 
     def check_for_winner(self, chip):
         """ Check all lines (Horizontal, Vertical, Positive Diagonals, Negative Diagonals) in board to find winner."""
@@ -54,12 +77,14 @@ class Game:
             for row in range(self.rows):
                 # if, for a given row, there are 5 of the same chip on subsequent columns, a player has won
                 if (
-                    self.board[col][row] == chip
-                    and self.board[col+1][row] == chip 
+                    self.board[col][row] == chip 
+                    and self.board[col+1][row] == chip  
                     and self.board[col+2][row] == chip 
-                    and self.board[col+3][row] == chip 
-                    and self.board[col+4][row] == chip 
-                ): return True
+                    and self.board[col+3][row] == chip  
+                    and self.board[col+4][row] == chip
+                ): 
+                    self.has_winner = True 
+                    return True
 
         # Check vertical
         # Same as above, but we flip the columns and rows
@@ -71,7 +96,9 @@ class Game:
                     and self.board[col][row+2] == chip 
                     and self.board[col][row+3] == chip 
                     and self.board[col][row+4] == chip 
-                ): return True
+                ): 
+                    self.has_winner = True 
+                    return True
 
         # Check positive diagonals
         for col in range(self.columns - 4):
@@ -83,7 +110,9 @@ class Game:
                     and self.board[col+2][row+2] == chip 
                     and self.board[col+3][row+3] == chip 
                     and self.board[col+4][row+4] == chip 
-                ): return True
+                ): 
+                    self.has_winner = True 
+                    return True
 
         # Check negative diagonals
         for col in range(self.columns):
@@ -95,4 +124,6 @@ class Game:
                     and self.board[col+2][row-2] == chip 
                     and self.board[col+3][row-3] == chip 
                     and self.board[col+4][row-4] == chip 
-                ): return True
+                ): 
+                    self.has_winner = True 
+                    return True
