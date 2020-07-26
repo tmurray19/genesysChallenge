d822d9bb4d79d31e253f94528c4c1bf0f5457ea8
Taidgh Murray <me@taidghmurray.ie>
2020-07-26 19:20:28 +0100

    Adding custom errors for other invalid game states

--- connect_five_errors.py
+++ connect_five_errors.py
@@ -1,10 +1,16 @@
 # Some custom errors for invalid game states
-class Error(Exception):
+class InvalidGameStateError(Exception):
     """Base class for other exceptions"""
     pass
 
-class BoardFullErorr(Error):
+class BoardFullErorr(InvalidGameStateError):
     """For when a board's column is full"""
     pass
 
+class NoNumberInputError(InvalidGameStateError):
+    """For when a player does not input a parsable number"""
+    pass
 
+class NumberOutOfBoundsError(InvalidGameStateError):
+    """For when a player inputs a number outside the bounds of the board"""
+    pass
